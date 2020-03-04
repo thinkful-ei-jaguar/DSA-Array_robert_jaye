f69acdedfaadf716ef5e6e002df71eb5d918e93c
Jaye Anne Laguardia <jayelaguardia@gmail.com>
2020-03-04 12:15:50 -0800

    kinda

--- drills.js
+++ drills.js
@@ -24,4 +24,28 @@ function main(){
 }
 
 
-main();
+function lessthan5(array) {
+  let result = []
+  for(let i = 0; i < array.length; i++){
+    if(array[i] > 5) {
+      result.push(array[i])
+    }
+  }
+  return result;
+}
+
+//console.log(lessthan5([7,7,7,8,8,1,2,3,6]))
+
+function product(array){
+  let result = [];
+  for(let i = 0; i < array.length; i++){
+    //get product
+    let product = 0;
+    let smol; // = every otheer number in array except current index
+    //iterate through smol array
+      product *= smol[i]
+    //save product
+    result.push(product)
+  }
+}
+//console.log(product([1, 3, 9, 4]))
